732d1494f51a275911a72e1b13337f82058db53b
cerealk <granaglia@gmail.com>
2013-09-11 14:25:45 +0200

    eliminata gestione actionbar

--- CyberDeck/src/it/ck/cyberdeck/CardDetailActivity.java
+++ CyberDeck/src/it/ck/cyberdeck/CardDetailActivity.java
@@ -22,9 +22,6 @@ public class CardDetailActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_card_detail);
 
-		// Show the Up button in the action bar.
-//		getActionBar().setDisplayHomeAsUpEnabled(true);
-
 		if (savedInstanceState == null) {
 			// Create the detail fragment and add it to the activity
 			// using a fragment transaction.
@@ -35,21 +32,4 @@ public class CardDetailActivity extends FragmentActivity {
 		}
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpTo(this,
-					new Intent(this, CardListActivity.class));
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
