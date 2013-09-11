12a83205aa55c8710466fb54db217a6c04c3146f
cerealk <granaglia@gmail.com>
2013-09-11 09:40:21 +0200

    eliminati riferimenti all'ImageService

--- CyberDeck/src/it/ck/cyberdeck/presentation/presenter/CardDetailPresenter.java
+++ CyberDeck/src/it/ck/cyberdeck/presentation/presenter/CardDetailPresenter.java
@@ -4,19 +4,14 @@ import it.ck.cyberdeck.model.Card;
 import it.ck.cyberdeck.presentation.CardDetailView;
 import it.ck.cyberdeck.presentation.DownloaderView;
 import it.ck.cyberdeck.presentation.service.ImageDownloader;
-import it.ck.cyberdeck.presentation.service.ImageService;
-import android.graphics.Bitmap;
 
 public class CardDetailPresenter {
 
 	private CardDetailView cardDetailFragment;
 	private Card card;
-	private ImageService imgService;
 
-	public CardDetailPresenter(CardDetailView cardDetailFragment,
-			ImageService imgService) {
+	public CardDetailPresenter(CardDetailView cardDetailFragment) {
 		this.cardDetailFragment = cardDetailFragment;
-		this.imgService = imgService;
 		setCard();
 	}
 
@@ -26,14 +21,9 @@ public class CardDetailPresenter {
 
 	public void populateView() {
 		if (card != null) {
-			Bitmap cardImage = imgService.getCardImage(card.getKey());
-			if (cardImage == null){
-				String url = "http://netrunnercards.info/web/bundles/netrunnerdbcards/images/cards/300x418/"+ card.getKey().getCardCode() +".png";
-				ImageDownloader downloader = new ImageDownloader(url , (DownloaderView)cardDetailFragment, card.getKey());
-				downloader.execute();
-			} else {
-				cardDetailFragment.setCardImage(cardImage);
-			}
+			String url = "http://netrunnercards.info/web/bundles/netrunnerdbcards/images/cards/300x418/"+ card.getKey().getCardCode() +".png";
+			ImageDownloader downloader = new ImageDownloader(url , (DownloaderView)cardDetailFragment, card.getKey());
+			downloader.execute();
 		}
 
 	}
