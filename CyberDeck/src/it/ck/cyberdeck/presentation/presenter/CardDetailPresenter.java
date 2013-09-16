69c657875a737cb7badf3d991295744f4bfbbaba
cerealk <granaglia@gmail.com>
2013-09-16 15:41:28 +0200

    rinominato campo

--- CyberDeck/src/it/ck/cyberdeck/presentation/presenter/CardDetailPresenter.java
+++ CyberDeck/src/it/ck/cyberdeck/presentation/presenter/CardDetailPresenter.java
@@ -7,22 +7,22 @@ import it.ck.cyberdeck.presentation.service.ImageDownloader;
 
 public class CardDetailPresenter {
 
-	private CardDetailView cardDetailFragment;
+	private CardDetailView cardDetailView;
 	private Card card;
 
 	public CardDetailPresenter(CardDetailView cardDetailFragment) {
-		this.cardDetailFragment = cardDetailFragment;
+		this.cardDetailView = cardDetailFragment;
 		setCard();
 	}
 
 	private void setCard() {
-		card = cardDetailFragment.getCard();
+		card = cardDetailView.getCard();
 	}
 
 	public void populateView() {
 		if (card != null) {
 			String url = "http://netrunnercards.info/web/bundles/netrunnerdbcards/images/cards/300x418/"+ card.getKey().getCardCode() +".png";
-			ImageDownloader downloader = new ImageDownloader(url , (DownloaderView)cardDetailFragment, card.getKey());
+			ImageDownloader downloader = new ImageDownloader(url , (DownloaderView)cardDetailView, card.getKey());
 			downloader.execute();
 		}
 
