bb868dcda5117ff708cad701c2dd3d9f6d6c669f
cerealk <granaglia@gmail.com>
2013-10-04 09:21:46 +0200

    cambiata gerarchia

--- CyberDeck/test/it/ck/cyberdeck/presentation/TestCyberDeckApp.java
+++ CyberDeck/test/it/ck/cyberdeck/presentation/TestCyberDeckApp.java
@@ -2,6 +2,6 @@ package it.ck.cyberdeck.presentation;
 
 import android.app.Application;
 
-public class TestCyberDeckApp extends Application {
+public class TestCyberDeckApp extends CyberDeckApp {
 
 }
