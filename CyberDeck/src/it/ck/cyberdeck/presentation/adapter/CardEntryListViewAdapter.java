d7b5f400b3e078a836bcd54ee0aaf74dd044e2e8
cerealk <granaglia@gmail.com>
2013-10-14 13:25:39 +0200

    Revert "inserito costo in reputazione nella entryList"
    This reverts commit 34887fe12f1f01d4e993a1083c8ae5c2c1422a29.

--- CyberDeck/src/it/ck/cyberdeck/presentation/adapter/CardEntryListViewAdapter.java
+++ CyberDeck/src/it/ck/cyberdeck/presentation/adapter/CardEntryListViewAdapter.java
@@ -1,15 +1,11 @@
 package it.ck.cyberdeck.presentation.adapter;
 
-import org.apache.commons.lang3.StringUtils;
-
 import it.ck.cyberdeck.R;
 import it.ck.cyberdeck.model.CardEntry;
-import it.ck.cyberdeck.model.Deck;
 import it.ck.cyberdeck.presentation.util.ImageFactory;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Typeface;
 import android.view.*;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -18,14 +14,10 @@ import android.widget.TextView;
 public class CardEntryListViewAdapter extends ArrayAdapter<CardEntry> {
 
 	private Context context;
-	private Deck deck;
-	private Typeface font;
 
-	public CardEntryListViewAdapter(Context context, Deck deck){
+	public CardEntryListViewAdapter(Context context){
 		super(context, R.layout.deck_entry_layout);
 		this.context = context;
-		this.deck = deck;
-		this.font = Typeface.createFromAsset( context.getAssets(), "fontawesome-webfont.ttf" );
 	}
 	
 	@Override
@@ -36,34 +28,14 @@ public class CardEntryListViewAdapter extends ArrayAdapter<CardEntry> {
 		View rowView = inflater.inflate(R.layout.deck_entry_layout, parent, false);
 		
 		ImageView icon = (ImageView) rowView.findViewById(R.id.icon);
-		TextView cardName = (TextView) rowView.findViewById(R.id.cardName);
-		TextView cardReputation = (TextView) rowView.findViewById(R.id.reputation);
-		cardReputation.setTypeface(font);
+		TextView textView = (TextView) rowView.findViewById(R.id.cardName);
 		TextView countText = (TextView) rowView.findViewById(R.id.countText);
 		CardEntry entry = getItem(position);
-		cardName.setText(getCardNameText(entry));
-		cardReputation.setText(getCardReputationText(entry));
-		countText.setText(getCardCountText(entry));
+		textView.setText(entry.getCard().getName());
+		countText.setText(String.valueOf(entry.getCount()));
 		Bitmap bmp = ImageFactory.getFactionImage(context, entry.getCard().getSide(), entry.getCard().getFaction());
 		icon.setImageBitmap(bmp);
 		return rowView;
-	}
-
-	private CharSequence getCardReputationText(CardEntry entry) {
-		Integer repoCost = deck.getIdentity().calculateReputationCost(entry.getCard());
-		if (repoCost > 0){
-			return StringUtils.repeat(context.getResources().getString(R.string.reputation_dot), repoCost);
-		}
-			
-		return "";
-	}
-
-	private String getCardCountText(CardEntry entry) {
-		return String.valueOf(entry.getCount());
-	}
-
-	private String getCardNameText(CardEntry entry) {
-		return entry.getCard().getName();
 	}
 	
 }
