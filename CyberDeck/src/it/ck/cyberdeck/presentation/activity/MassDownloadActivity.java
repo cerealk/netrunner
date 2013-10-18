8153d593b59823bb29890ebeea4d2d146e8cd5d9
cerealk <granaglia@gmail.com>
2013-10-18 12:53:04 +0200

    fixata la continua esecuzione del task

--- CyberDeck/src/it/ck/cyberdeck/presentation/activity/MassDownloadActivity.java
+++ CyberDeck/src/it/ck/cyberdeck/presentation/activity/MassDownloadActivity.java
@@ -3,27 +3,98 @@ package it.ck.cyberdeck.presentation.activity;
 import it.ck.cyberdeck.R;
 import it.ck.cyberdeck.model.CardLibrary;
 import it.ck.cyberdeck.presentation.BaseCardLibraryActivity;
+import it.ck.cyberdeck.presentation.service.AndroidFSImageService;
+import it.ck.cyberdeck.presentation.service.ImageService;
 import it.ck.cyberdeck.presentation.service.MassDownloadTask;
+import android.os.AsyncTask.Status;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.widget.TextView;
 
 public class MassDownloadActivity extends BaseCardLibraryActivity {
 
-	private CardLibrary cardLibrary;
+	
+	private static final String TAG = "trf";
 	private TextView advancement;
+	private CardLibrary cardLibrary;
 
+	private TaskRetainFragment trf;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mass_download);
 		advancement = (TextView) findViewById(R.id.textView1);
 		cardLibrary = getCardLibrary();
-		MassDownloadTask task = new MassDownloadTask(this, cardLibrary);
-		task.execute();
+		ImageService is = new AndroidFSImageService(this);
+		trf = findOrCreateRetainFragment(getSupportFragmentManager());
+		trf.createMassDownloadTask(is, cardLibrary, advancement);
+		trf.executeTask();
+		trf.setUpdateView(advancement);
+
 	}
 
 	public void updateProgress(Integer perc) {
 		advancement.setText(perc.toString() + "%");
 	}
+	
+	private static TaskRetainFragment findOrCreateRetainFragment(FragmentManager fm) {
+        // Check to see if we have retained the worker fragment.
+		TaskRetainFragment mRetainFragment = (TaskRetainFragment) fm.findFragmentByTag(TAG);
+
+        // If not retained (or first time running), we need to create and add it.
+        if (mRetainFragment == null) {
+            mRetainFragment = new TaskRetainFragment();
+            fm.beginTransaction().add(mRetainFragment, TAG).commitAllowingStateLoss();
+        }
+
+        return mRetainFragment;
+    }
+	
+    public static class TaskRetainFragment extends Fragment {
+        private MassDownloadTask task;
+
+        /**
+         * Empty constructor as per the Fragment documentation
+         */
+        public TaskRetainFragment() {}
+
+        public void setUpdateView(TextView advancement) {
+			task.setProgressView(advancement);
+			
+		}
+
+		public void executeTask() {
+        	Status status = task.getStatus();
+        	if(status.equals(Status.PENDING)){
+        		task.execute();
+        	}
+		}
+
+		@Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setRetainInstance(true);
+        }
+
+        /**
+         * Store a single object in this Fragment.
+         *
+         * @param object The object to store
+         */
+        public void createMassDownloadTask(ImageService is, CardLibrary cl, TextView advancement) {
+            if(task == null)
+            	task = new MassDownloadTask(is, cl, advancement);
+        }
+
+        /**
+         * Get the stored object.
+         *
+         * @return The stored object
+         */
+        public MassDownloadTask getObject() {
+            return task;
+        }
+    }
 
 }
--- CyberDeck/src/it/ck/cyberdeck/presentation/service/MassDownloadTask.java
+++ CyberDeck/src/it/ck/cyberdeck/presentation/service/MassDownloadTask.java
@@ -1,33 +1,37 @@
 package it.ck.cyberdeck.presentation.service;
 
-import java.util.List;
-
 import it.ck.cyberdeck.model.Card;
 import it.ck.cyberdeck.model.CardLibrary;
-import it.ck.cyberdeck.presentation.activity.MassDownloadActivity;
+
+import java.util.List;
+
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 public class MassDownloadTask extends AsyncTask<Void, Integer, Void>{
 
-	private MassDownloadActivity activity;
 	private CardLibrary cardLibrary;
+	private ImageService is;
+	private TextView progress;
+	private int clSize;
 
-	public MassDownloadTask (MassDownloadActivity activity, CardLibrary cardLibrary){
-		this.activity = activity;
+	public MassDownloadTask (ImageService is, CardLibrary cardLibrary, TextView progress){
+		this.is = is;
 		this.cardLibrary = cardLibrary;
-		
+		this.progress = progress;
+		this.clSize = cardLibrary.getCardList().size();
 	}
 	
 	@Override
 	protected Void doInBackground(Void... params) {
 		List<Card> cards = cardLibrary.getCardList();
-		ImageService is = new AndroidFSImageService(activity);
 		int i=0;
 		for(Card card: cards){
 			
 			is.getCardImage(card.getKey());
 			i++;
-			activity.updateProgress(getWorkDone(i));
+			
+			publishProgress(getWorkDone(i));
 		}
 		
 		
@@ -35,7 +39,19 @@ public class MassDownloadTask extends AsyncTask<Void, Integer, Void>{
 	}
 
 	private Integer getWorkDone(int i) {
-		return i/cardLibrary.getCardList().size() *100;
+		
+		return i * 100 / clSize ;
+	}
+	
+	@Override
+	protected void onProgressUpdate(Integer... values) {
+		progress.setText(values[0].toString());
+		super.onProgressUpdate(values);
+	}
+
+	public void setProgressView(TextView advancement) {
+		this.progress = advancement;
+		
 	}
 
 }
