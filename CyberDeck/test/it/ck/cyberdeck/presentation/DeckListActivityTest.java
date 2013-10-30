0a18cb0205343cfa08d44152f97106ed4e85c916
cerealk <granaglia@gmail.com>
2013-10-30 10:14:32 +0100

    ignorato test non ancora implementato

--- CyberDeck/test/it/ck/cyberdeck/presentation/DeckListActivityTest.java
+++ CyberDeck/test/it/ck/cyberdeck/presentation/DeckListActivityTest.java
@@ -2,11 +2,13 @@ package it.ck.cyberdeck.presentation;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class DeckListActivityTest {
 
 	@Test
+	@Ignore
 	public void test() {
 		fail("Not yet implemented");
 	}
