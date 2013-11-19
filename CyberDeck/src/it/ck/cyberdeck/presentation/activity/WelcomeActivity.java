9e49947f1f941ada683d6780cd3c858393721cc4
cerealk <granaglia@gmail.com>
2013-11-19 17:00:46 +0100

    eliminato menu

--- CyberDeck/src/it/ck/cyberdeck/presentation/activity/WelcomeActivity.java
+++ CyberDeck/src/it/ck/cyberdeck/presentation/activity/WelcomeActivity.java
@@ -4,9 +4,6 @@ import it.ck.cyberdeck.R;
 import it.ck.cyberdeck.presentation.BaseCyberDeckActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -50,13 +47,6 @@ public class WelcomeActivity extends BaseCyberDeckActivity {
 			}
 
 		});
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.welcome_menu, menu);
-	    return true;
 	}
 	
 }
