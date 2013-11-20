49500157c74e50a111a7aa58df5d59a2acf488a4
cerealk <granaglia@gmail.com>
2013-11-20 10:30:12 +0100

    format

--- CyberDeck/src/it/ck/cyberdeck/model/LibraryCardGateway.java
+++ CyberDeck/src/it/ck/cyberdeck/model/LibraryCardGateway.java
@@ -2,14 +2,13 @@ package it.ck.cyberdeck.model;
 
 import java.util.List;
 
-
 public interface LibraryCardGateway {
 
 	CardLibrary loadCardLibrary();
-	
-  void saveDeck(Deck deck);
-  
-  Deck loadDeck(String name);
+
+	void saveDeck(Deck deck);
+
+	Deck loadDeck(String name);
 
 	List<String> deckNames();
 
