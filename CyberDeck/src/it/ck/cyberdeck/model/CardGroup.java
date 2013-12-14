e13eb64876add929c26306c543b3224da4be6f14
cerealk <granaglia@gmail.com>
2013-12-14 14:34:05 +0100

    format

--- CyberDeck/src/it/ck/cyberdeck/model/CardGroup.java
+++ CyberDeck/src/it/ck/cyberdeck/model/CardGroup.java
@@ -8,26 +8,26 @@ import java.util.*;
 public class CardGroup implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-  
+
 	private CardType type;
-  
-  private List<Card> cards = new ArrayList<Card>();
-	
-  public CardGroup(CardType type) {
+
+	private List<Card> cards = new ArrayList<Card>();
+
+	public CardGroup(CardType type) {
 		this.type = type;
-  }
-  
-  public void add(Card card){
-  	cards.add(card);
-  }
-  
-  public CardType getType(){
-  	return this.type;
-  }
-  
-  public List<Card> getCards(){
-	Collections.sort(cards, new CardKeyComparator());
-  	return Collections.unmodifiableList(cards);
-  }
+	}
+
+	public void add(Card card) {
+		cards.add(card);
+	}
+
+	public CardType getType() {
+		return this.type;
+	}
+
+	public List<Card> getCards() {
+		Collections.sort(cards, new CardKeyComparator());
+		return Collections.unmodifiableList(cards);
+	}
 
 }
