08223c6e2b3ec87c5683c851ff18f81889a3ceae
cerealk <granaglia@gmail.com>
2014-02-02 16:17:56 +0100

    test comandi

--- CyberDeck/test/it/ck/cyberdeck/model/command/AddCardCommandTest.java
+++ CyberDeck/test/it/ck/cyberdeck/model/command/AddCardCommandTest.java
@@ -7,24 +7,39 @@ import static it.ck.cyberdeck.fixtures.CardTestFactory.*;
 import static org.hamcrest.Matchers.*;
 import it.ck.cyberdeck.model.Card;
 import it.ck.cyberdeck.model.Deck;
+import it.ck.cyberdeck.model.DeckException;
+import it.ck.cyberdeck.model.Notifier;
 
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class AddCardCommandTest {
 
+    @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+	private Notifier notifier = context.mock(Notifier.class);
+	
 	public class AddCardCommand implements Command {
 
 		private Card card;
 		private Deck deck;
+		private Notifier notifier;
 
-		public AddCardCommand(Card card, Deck deck) {
+		public AddCardCommand(Card card, Deck deck, Notifier notifier) {
 			this.card = card;
 			this.deck = deck;
+			this.notifier = notifier;
 		}
 
 		@Override
 		public void execute() {
-			deck.add(card);
+			try {
+				deck.add(card);
+				this.notifier.notify(card.getName() + " added");
+			} catch (DeckException e) {
+				this.notifier.notify(e.getMessage());
+			}
 		}
 
 	}
@@ -33,9 +48,53 @@ public class AddCardCommandTest {
 	public void givenAnEmptyDeckICanAddACard() throws Exception {
 		Deck deck = getEmptyDeck();
 		Card card = getCard();
-		Command addCommand = new AddCardCommand(card, deck);
+		Command addCommand = new AddCardCommand(card, deck, getDoNothingNotifier());
 		addCommand.execute();
 		assertThat(deck.size(), is(1));
+	}
+
+	@Test
+	public void givenADeckICanAddACard() throws Exception {
+		Deck deck = twoCardDeck();
+		Command addCardCommand = new AddCardCommand(getCard(), deck, getDoNothingNotifier());
+		addCardCommand.execute();
+		assertThat(deck.size(), is(3));
+	}
+	
+	@Test
+	public void whenACardIsAddedTheUserIsNotified() throws Exception {
+		context.checking(new Expectations(){{
+			oneOf(notifier).notify("std added");
+		}});
+		
+		Deck deck = twoCardDeck();
+		Command addCardCommand = new AddCardCommand(getCard(), deck, notifier);
+		addCardCommand.execute();
+		assertThat(deck.size(), is(3));
+	}
+	
+	@Test
+	public void anErrorInTheoperationIsCorrectlyNotified() throws Exception {
+		
+		
+		context.checking(new Expectations(){{
+			oneOf(notifier).notify("std added");
+			oneOf(notifier).notify("card limit exceeded");
+		}});
+		
+		Deck deck = twoCardDeck();
+		Command addCardCommand = new AddCardCommand(getCard(), deck, notifier);
+		addCardCommand.execute();
+		addCardCommand.execute();
+		assertThat(deck.size(), is(3));
+	}
+	
+	private Notifier getDoNothingNotifier() {
+		return new Notifier() {
+			@Override
+			public void notify(String message) {
+			}
+		};
 	}
 	
 }
