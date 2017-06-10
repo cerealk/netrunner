70bdb42ac152de1ebb30b0b42aeca9a7a38cdc01
cerealk <granaglia@gmail.com>
2017-06-10 14:16:27 +0200

    minor cosmetich changes

--- CyberDeck/src/main/java/it/ck/cyberdeck/presentation/util/ImageLoaderFactory.java
+++ CyberDeck/src/main/java/it/ck/cyberdeck/presentation/util/ImageLoaderFactory.java
@@ -8,9 +8,8 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 public class ImageLoaderFactory {
 
 	private ImageLoader imageLoader = ImageLoader.getInstance();
-	
+
 	public void display(CardKey key, ImageView identityImg){
-//		String urlString = "http://netrunnerdb.com/web/bundles/netrunnerdbcards/images/cards/en/"+ key.getCardCode() +".png";
 		String urlString = "https://netrunnerdb.com/card_image/"+ key.getCardCode() +".png";
 		imageLoader.displayImage(urlString, identityImg);
 	}
